43cb71512a4f404d52c90efaa8e6e4249f4fa940
Surya Umapathy <suryaumapathy2812@gmail.com>
2023-03-09 16:21:41 +0530

    updated app.js

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -8,12 +8,5 @@ function charLength() {
 
 }
 
-// let charLengthNum = parseInt(charLength());
-
-// document.getElementById('length').addEventListener('change', function () {
-//     charLengthNum = document.getElementById('length').value;
-//     document.getElementById('result').innerHTML = charLength;
-//     charLengthNum = parseInt(charLengthNum);
-//     return charLength;
-// });
+document.getElementById('length').addEventListener('change', charLength)
 
