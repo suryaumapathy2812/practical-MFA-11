7a3799ba6777046e093a253af48cd10dadc58a8f
Surya Umapathy <suryaumapathy2812@gmail.com>
2023-03-09 12:57:39 +0530

    First commit

---
+++ assets/JS/app.js
@@ -0,0 +1,19 @@
+
+/*get the number of characters*/
+function charLength() {
+
+    let charLength = document.getElementById('length').value;
+    document.getElementById('result').innerHTML = charLength;
+    return charLength;
+
+}
+
+// let charLengthNum = parseInt(charLength());
+
+// document.getElementById('length').addEventListener('change', function () {
+//     charLengthNum = document.getElementById('length').value;
+//     document.getElementById('result').innerHTML = charLength;
+//     charLengthNum = parseInt(charLengthNum);
+//     return charLength;
+// });
+
